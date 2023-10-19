06da326536defc29f22da72578a97110aa4ab4da
Stijn van Houwelingen <stijn@van-houwelingen.net>
2023-10-19 22:03:18 +0200

    refactor: Added testing. Moved structs and validation functions into seperate files

---
+++ main_test.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/gavv/httpexpect/v2"
+	"github.com/gin-gonic/gin"
+)
+
+// Rid of debug output
+func init() {
+	gin.SetMode(gin.TestMode)
+}
+
+var correctUser = map[string]interface{}{
+	"legalfirstnames": "boben b",
+	"member": map[string]interface{}{
+		"firstname": "bob",
+		"infix":     "de",
+		"lastname":  "tak",
+		"phone":     "+31612345678",
+	},
+	"date_of_birth": "2000-10-12T00:00:00Z",
+	"address":       "Lovensdijkstaat 16",
+	"postal_code":   "4793RR",
+	"city":          "Breda",
+	"email":         "jandevries@example.org",
+	"course":        "TI",
+	"cohort":        "2022/2023",
+	"emergency_contact": map[string]interface{}{
+		"firstname": "greetje",
+		"infix":     "de",
+		"lastname":  "vries",
+		"phone":     "+31687654321",
+	},
+	"iban":           "NL18RABO0123459876",
+	"account_holder": "B. B. de Tak",
+}
+
+func getGinHandler(t *testing.T) *httpexpect.Expect {
+	// Create new gin instance
+	handler := initRouter()
+	// Create httpexpect instance
+	return httpexpect.WithConfig(httpexpect.Config{
+		Client: &http.Client{
+			Transport: httpexpect.NewBinder(handler),
+			Jar:       httpexpect.NewCookieJar(),
+		},
+		Reporter: httpexpect.NewAssertReporter(t),
+		Printers: []httpexpect.Printer{
+			httpexpect.NewDebugPrinter(t, true),
+		},
+	})
+}
+
+func TestSignupShouldReturnSuccessWhenUserIsCorrect(t *testing.T) {
+	// Arrange
+	e := getGinHandler(t)
+
+	// Act & Assert
+	e.POST("/signup").
+		WithJSON(correctUser).
+		Expect().
+		Status(http.StatusOK).JSON().
+		Object().HasValue("Success", "Registration successful.")
+}
+
+func TestSignupShouldReturnErrorWhenPostalCodeIsInvalid(t *testing.T) {
+	// Arrange
+	e := getGinHandler(t)
+	userWithIncorrectPostalcodeNumbers := correctUser
+	userWithIncorrectPostalcodeLetters := correctUser
+
+	userWithIncorrectPostalcodeNumbers["postal_code"] = "132NV"
+	userWithIncorrectPostalcodeLetters["postal_code"] = "1323N"
+
+	// Act & Assert
+	e.POST("/signup").
+		WithJSON(userWithIncorrectPostalcodeNumbers).
+		Expect().
+		Status(http.StatusBadRequest).JSON().
+		Object().HasValue("Error", "Postcode is onjuist, probeer het zo: 4818 AJ.")
+
+	e.POST("/signup").
+		WithJSON(userWithIncorrectPostalcodeLetters).
+		Expect().
+		Status(http.StatusBadRequest).JSON().
+		Object().HasValue("Error", "Postcode is onjuist, probeer het zo: 4818 AJ.")
+}
